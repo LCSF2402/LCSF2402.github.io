a74b38fd657f55d7f4eee4f7cb9f789689780583
JohnnyCM <c0944569@mylambton.ca>
2024-12-06 07:14:24 -0500

    Term Project

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+/*
+* Author: Johnny Castillo
+* File name: script.js
+* Date: 12/05/2024
+*/
+
+// Hamburger menu function
+function hamburger() {
+    const menuLinks = document.getElementById('menu-link');
+    menuLinks.style.display = menuLinks.style.display === 'block' ? 'none' : 'block';
+}
+
+function hamburger() {
+    const mobileNav = document.querySelector('.mobile-nav');
+    mobileNav.classList.toggle('active');
+}
+
